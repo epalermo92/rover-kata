224fe50d3ac428a9221ce1946060ff36c5d90cbb
Gianfranco <gianfranco.pagliaroto@facile.it>
2019-12-16 17:47:40 +0100

    Command code refactoring pt 1

--- src/Functions/Command.php
+++ src/Functions/Command.php
@@ -57,25 +57,28 @@ class Command extends AbstractCommand
 
     protected static function move(Rover $rover, Mars $mars, AbstractCommand $command): Rover
     {
-        $case = $command->getCommand() . $rover->getDirection()->getDirectionString();
-        $delta = self::coordinatesCombination($case);
+        $combination = $command->getCommand() . $rover->getDirection()->getDirectionString();
 
-        $newRover = [
-            'FN' => new Rover(self::buildPosition($rover->getPosition()->getX(), $rover->getPosition()->getY() + $delta), self::buildDirection($case[1])),
-            'FS' => new Rover(self::buildPosition($rover->getPosition()->getX(), $rover->getPosition()->getY() + $delta), self::buildDirection($case[1])),
-            'BN' => new Rover(self::buildPosition($rover->getPosition()->getX(), $rover->getPosition()->getY() + $delta), self::buildDirection($case[1])),
-            'BS' => new Rover(self::buildPosition($rover->getPosition()->getX(), $rover->getPosition()->getY() + $delta), self::buildDirection($case[1])),
-            'FE' => new Rover(self::buildPosition($rover->getPosition()->getX() + $delta, $rover->getPosition()->getY()), self::buildDirection($case[1])),
-            'FW' => new Rover(self::buildPosition($rover->getPosition()->getX() + $delta, $rover->getPosition()->getY()), self::buildDirection($case[1])),
-            'BE' => new Rover(self::buildPosition($rover->getPosition()->getX() + $delta, $rover->getPosition()->getY()), self::buildDirection($case[1])),
-            'BW' => new Rover(self::buildPosition($rover->getPosition()->getX() + $delta, $rover->getPosition()->getY()), self::buildDirection($case[1])),
+        $direction = $rover->getDirection();
+        $x = $rover->getPosition()->getX();
+        $y = $rover->getPosition()->getY();
+
+        $cases = [
+            'FN' => [$x, $y + 1],
+            'FS' => [$x, $y - 1],
+            'BN' => [$x, $y - 1],
+            'BS' => [$x, $y + 1],
+            'FE' => [$x + 1, $y],
+            'FW' => [$x - 1, $y],
+            'BE' => [$x - 1, $y],
+            'BW' => [$x + 1, $y],
         ];
 
-        if ($mars->getObstacles() && self::meetsObstacles($newRover[$case]->getPosition(), $mars->getObstacles())) {
+        if ($mars->getObstacles() && self::meetsObstacles(new Position(...$cases[$combination]), $mars->getObstacles())) {
             return $rover;
         }
 
-        return $newRover[$case];
+        return new Rover(new Position(...$cases[$combination]),$direction);
     }
 
     protected static function turn(Rover $rover, AbstractCommand $command): Rover
@@ -91,30 +94,7 @@ class Command extends AbstractCommand
             $relations = array_flip($relations);
         }
 
-        $newRover = [
-            DirectionW::class => new Rover(self::buildPosition($rover->getPosition()->getX(), $rover->getPosition()->getY()), new $relations[get_class(new DirectionW())]),
-            DirectionS::class => new Rover(self::buildPosition($rover->getPosition()->getX(), $rover->getPosition()->getY()), new $relations[get_class(new DirectionS())]),
-            DirectionE::class => new Rover(self::buildPosition($rover->getPosition()->getX(), $rover->getPosition()->getY()), new $relations[get_class(new DirectionE())]),
-            DirectionN::class => new Rover(self::buildPosition($rover->getPosition()->getX(), $rover->getPosition()->getY()), new $relations[get_class(new DirectionN())]),
-        ];
-
-        return $newRover[get_class($rover->getDirection())];
-    }
-
-    public static function coordinatesCombination(string $case): int
-    {
-        $caseSelector = [
-            'FN' => 1,
-            'FE' => 1,
-            'BS' => 1,
-            'BW' => 1,
-            'FS' => -1,
-            'FW' => -1,
-            'BN' => -1,
-            'BE' => -1,
-        ];
-
-        return $caseSelector[$case];
+        return new Rover(new Position($rover->getPosition()->getX(), $rover->getPosition()->getY()), new $relations[get_class($rover->getDirection())]);
     }
 
     private static function meetsObstacles(Position $position, array $obstacles): bool
@@ -125,32 +105,5 @@ class Command extends AbstractCommand
             }
         }
         return false;
-    }
-
-    public static function buildPosition(int $x, int $y):Position{
-        return PositionBuilder::build(
-            $x,
-            $y,
-            )
-            ->either(
-                static function (\Exception $e) {
-                    echo 'Input Error.' . $e->getMessage();
-                },
-                static function (Position $position){
-                    return $position;
-                });
-    }
-
-    public static function buildDirection(string $direction):AbstractDirection{
-        return DirectionBuilder::build(
-            $direction
-            )
-            ->either(
-                static function (\Exception $e) {
-                    echo 'Input Error.' . $e->getMessage();
-                },
-                static function (AbstractDirection $direction){
-                    return $direction;
-                });
     }
 }
