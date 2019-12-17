c5d3489b95c04804262b8b7a288cb667784f1c00
Gianfranco <gianfranco.pagliaroto@facile.it>
2019-12-17 17:07:26 +0100

    Tests completed

--- tests/Functions/CommandTest.php
+++ tests/Functions/CommandTest.php
@@ -15,12 +15,26 @@ use PHPUnit\Framework\TestCase;
 class CommandTest extends TestCase
 {
 
-    public function testExecuteCommandMove(): void
+    public function testExecuteCommandMove()
     {
-        //TODO
+        $mars = new Mars(
+            8,
+            8,
+            [new Position(1, 3), new Position(2, 5)]
+        );
+        $rover = new Rover(new Position(2, 2), new DirectionN());
+        $command = [new CommandF()];
+
+        $result = Command::executeCommand($mars, $rover, $command);
+
+        $this->assertSame(2, $result->getPosition()->getX());
+        $this->assertSame(3, $result->getPosition()->getY());
+        $this->assertSame(DirectionN::class, get_class($result->getDirection()));
+
+
     }
 
-    public function testExecuteCommandTurn(): void
+    public function testExecuteCommandTurn()
     {
         /** @var Position[] $obstacles */
         $obstacles = [
