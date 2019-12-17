6ce766af95da4a2f9b8e266ffc011cd688ca7303
emanuele <emanuele.palermo@facile.it>
2019-12-17 17:04:28 +0100

    testExecuteCommandTurn done

--- tests/Functions/CommandTest.php
+++ tests/Functions/CommandTest.php
@@ -2,18 +2,37 @@
 
 namespace Unit;
 
+use App\Functions\Command;
+use App\Models\Command\CommandF;
+use App\Models\Command\CommandR;
+use App\Models\Direction\DirectionE;
+use App\Models\Direction\DirectionN;
+use App\Models\Mars;
+use App\Models\Position;
+use App\Models\Rover;
 use PHPUnit\Framework\TestCase;
 
 class CommandTest extends TestCase
 {
 
-    public function testExecuteCommandMove()
+    public function testExecuteCommandMove(): void
     {
         //TODO
     }
 
-    public function testExecuteCommandTurn()
+    public function testExecuteCommandTurn(): void
     {
-        //TODO
+        /** @var Position[] $obstacles */
+        $obstacles = [
+            new Position(0, 2)
+        ];
+
+        $mars = new Mars(4, 4, $obstacles);
+        $rover = new Rover(new Position(0, 0), new DirectionN());
+        $result = Command::executeCommand($mars, $rover, [
+            new CommandR()
+        ]);
+
+        $this->assertSame(DirectionE::class, get_class($result->getDirection()));
     }
 }
