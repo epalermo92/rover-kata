7867fcea8106cfd91fff22b8e73865bb7f4c46ce
emanuele <emanuele.palermo@facile.it>
2019-12-17 16:36:34 +0100

    initializing unit test

---
+++ tests/Functions/CommandTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class CommandTest extends TestCase
+{
+
+    public function testExecuteCommandMove()
+    {
+        //TODO
+    }
+
+    public function testExecuteCommandTurn()
+    {
+        //TODO
+    }
+}
